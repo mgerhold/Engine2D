ff7050f21fe4c1e468a9ba616ba87dd9a1094223
Michael Gerhold <michael.gerhold@web.de>
2021-08-16 16:45:16 +0200

    Removed unnecessary includes from main.cpp

--- src/Sandbox/main.cpp
+++ src/Sandbox/main.cpp
@@ -1,11 +1,5 @@
 #include "Sandbox.hpp"
-#include "SparseSet.hpp"
-#include "Registry.hpp"
-#include <range/v3/all.hpp>
 #include <cstdlib>
-#include <limits>
-#include <string>
-#include <tuple>
 
 int main() {
     // sandbox application
