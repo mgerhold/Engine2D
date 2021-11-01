f0ac0bd17187b27a7aae32215a18fcb46095a30e
Michael Gerhold <michael.gerhold@web.de>
2021-11-01 13:13:20 +0100

    put all C++ code into Main.cpp

--- src/FlappyBird/Main.cpp
+++ src/FlappyBird/Main.cpp
@@ -2,8 +2,22 @@
 // Created by coder2k on 27.10.2021.
 //
 
-#include "FlappyBird.hpp"
+#include <Application.hpp>
 #include <cstdlib>
+
+using namespace c2k;
+
+class FlappyBird : public Application {
+    void setup() noexcept override {
+        const auto scriptGUID = GUID::fromString("1b0b54bf-d36a-4067-bd60-cc69900bb9bc");
+        mAssetDatabase.loadFromList(AssetDatabase::assetPath() / "assets.json");
+        mRegistry.createEntity(ScriptComponent{ &mAssetDatabase.scriptMutable(scriptGUID) });
+        mRenderer.setClearColor(Color{ 113.0f / 255.0f, 197.0f / 255.0f, 207.0f / 255.0f, 1.0f });
+    }
+
+    void update() noexcept override { }
+};
+
 
 int main() {
     FlappyBird game;
